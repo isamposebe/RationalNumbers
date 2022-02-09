9f1496ac6063bb05d9d26d3c9ae5072f2e174361
andreymlv <ndrmlv@yandex.ru>
2022-02-09 14:12:27 +0300

    added validation to wpf

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Numerics;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WpfApp;
 
@@ -26,21 +28,26 @@ public partial class MainWindow : Window
         RadioButtonMultiplication.Checked += Calculate;
     }
 
+    private static bool Validate(TextBox box, out int value, bool isBot = false)
+    {
+        if ((!int.TryParse(box.Text.Trim(), out value)) || (isBot && value is 0))
+        {
+            box.Background = Brushes.Red;
+            return false;
+        }
+        box.Background = Brushes.White;
+        return true;
+    }
+
     private void Calculate(object sender, EventArgs e)
     {
-        var lTop = 1;
-        var lBot = 1;
-        var rTop = 1;
-        var rBot = 1;
-
-        if (!(int.TryParse(TextBoxLeftSideTop.Text.Trim(), out lTop) ||
-            int.TryParse(TextBoxLeftSideBot.Text.Trim(), out lBot) ||
-            int.TryParse(TextBoxRightSideTop.Text.Trim(), out rTop) ||
-            int.TryParse(TextBoxRightSideBot.Text.Trim(), out rBot)))
+        if (!(Validate(TextBoxLeftSideTop, out int lTop) &&
+            Validate(TextBoxLeftSideBot, out int lBot, true) &&
+            Validate(TextBoxRightSideTop, out int rTop) &&
+            Validate(TextBoxRightSideBot, out int rBot, true)))
         {
             return;
         }
-
 
         Rational left = new(
             new BigInteger(lTop),
@@ -51,11 +58,11 @@ public partial class MainWindow : Window
             new BigInteger(rBot)
         );
 
-        if (RadioButtonDivision.IsChecked == true)
+        if (RadioButtonDivision.IsChecked is true)
             LabelResult.Content = left / right;
-        else if (RadioButtonMinus.IsChecked == true)
+        else if (RadioButtonMinus.IsChecked is true)
             LabelResult.Content = left - right;
-        else if (RadioButtonMultiplication.IsChecked == true)
+        else if (RadioButtonMultiplication.IsChecked is true)
             LabelResult.Content = left * right;
         else
             LabelResult.Content = left + right;
